88584b87612e03957704d16aea72fd1cc88aca8b
ryo-yoshizawa <r2yoshizawa@lab.miraikan.jst.go.jp>
2023-02-14 11:56:36 +0900

    Accessibility support for radio button cells

--- Miraikan/Components/Parts/TableViewCell/RouteModeRow.swift
+++ Miraikan/Components/Parts/TableViewCell/RouteModeRow.swift
@@ -13,24 +13,53 @@ import UIKit
  Caution: This is related to but not the same as the modes in NavCog3
  */
 
-class RouteModeRow: BaseRow {
+class RouteModeRow: UITableViewCell {
     
+    private let baseView = UIView()
+    private let titleLabel = UILabel()
     private var radioGroup = [RouteMode: RadioButton]()
-    
-    // Sizing
-    private let gapX: CGFloat = 20
-    private let gapY: CGFloat = 16
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         self.selectionStyle = .none
         
+        setupBaseView()
+        setupTitleLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupBaseView() {
+        contentView.addSubview(baseView)
+
+        baseView.translatesAutoresizingMaskIntoConstraints = false
+        let widthConstraint = baseView.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.9)
+        let heightConstraint = baseView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.9)
+        let centerXConstraint = baseView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
+        let centerYConstraint = baseView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        NSLayoutConstraint.activate([widthConstraint, heightConstraint, centerXConstraint, centerYConstraint])
+    }
+
+    private func setupTitleLabel() {
+        titleLabel.isAccessibilityElement = false
+        titleLabel.font = .preferredFont(forTextStyle: .callout)
+        titleLabel.text = NSLocalizedString("Mode", comment: "")
+        baseView.addSubview(titleLabel)
+
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        let leading = titleLabel.leadingAnchor.constraint(equalTo: baseView.leadingAnchor, constant: 0)
+        let trailing = titleLabel.trailingAnchor.constraint(equalTo: baseView.trailingAnchor, constant: 0)
+        let top = titleLabel.topAnchor.constraint(equalTo: baseView.topAnchor, constant: 8)
+        NSLayoutConstraint.activate([leading, trailing, top])
+        titleLabel.setContentHuggingPriority(.defaultLow, for: .vertical)
+        
+        var anchor = titleLabel.bottomAnchor
         RouteMode.allCases.forEach({ mode in
             let btn = RadioButton()
-            btn.setTitle(mode.description, for: .normal)
-            btn.setTitleColor(.label, for: .normal)
-            btn.titleLabel?.font = .preferredFont(forTextStyle: .callout)
+            btn.setText(text: mode.description)
             btn.isChecked = mode == MiraikanUtil.routeMode
             btn.tapAction({ [weak self] _ in
                 guard let _self = self else { return }
@@ -43,39 +72,19 @@ class RouteModeRow: BaseRow {
                     UserDefaults.standard.setValue(mode.rawValue, forKey: "RouteMode")
                 }
             })
-            
             radioGroup[mode] = btn
-            addSubview(btn)
+            baseView.addSubview(btn)
+            
+            btn.translatesAutoresizingMaskIntoConstraints = false
+            let btnLeading = btn.leadingAnchor.constraint(equalTo: baseView.leadingAnchor, constant: 0)
+            let btnTrailing = btn.trailingAnchor.constraint(equalTo: baseView.trailingAnchor, constant: 0)
+            let btnTop = btn.topAnchor.constraint(equalTo: anchor, constant: 0)
+            NSLayoutConstraint.activate([btnLeading, btnTrailing, btnTop])
+            
+            anchor = btn.bottomAnchor
         })
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        var y = insets.top + gapY
-        RouteMode.allCases.forEach({ mode in
-            let btn = radioGroup[mode]!
-            let szFit = btn.sizeThatFits(innerSize)
-            btn.frame = CGRect(x: insets.left + gapX,
-                               y: y,
-                               width: szFit.width - gapX * 2,
-                               height: szFit.height + gapY)
-            y += btn.frame.height
-        })
-    }
 
-    override func sizeThatFits(_ size: CGSize) -> CGSize {
-
-        var y = insets.top + gapY
-        RouteMode.allCases.forEach({ mode in
-            let btn = radioGroup[mode]!
-            let szFit = btn.sizeThatFits(innerSize)
-            y += szFit.height + gapY
-        })
-        return CGSize(width: size.width, height: y)
+        let bottom = baseView.bottomAnchor.constraint(equalTo: anchor, constant: 0)
+        NSLayoutConstraint.activate([bottom])
     }
 }
